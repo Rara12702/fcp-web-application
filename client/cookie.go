89196e0ebda84e740bc0390fb3a3fe6c4b7ee5b0
Rara12702 <raradeninda12@gmail.com>
2023-06-25 20:39:11 +0700

    fcp web application cookie

--- client/cookie.go
+++ client/cookie.go
@@ -18,8 +18,8 @@ func GetClientWithCookie(token string, cookies ...*http.Cookie) (*http.Client, e
 	})
 
 	jar.SetCookies(&url.URL{
-		Scheme: "http",
-		Host:   "localhost:8080",
+		Scheme: "https",
+		Host:   "fcp-web-application-production-c9fc.up.railway.app",
 	}, cookies)
 
 	c := &http.Client{
